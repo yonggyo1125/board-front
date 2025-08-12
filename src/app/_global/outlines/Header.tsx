59d39f1b4ae03c4610f56acf1c3c623ef431436f
yonggyo1125 <yonggyo00@kakao.com>
2025-08-12 09:49:39 +0900

    .

--- src/app/_global/outlines/Header.tsx
+++ src/app/_global/outlines/Header.tsx
@@ -1,7 +1,28 @@
+'use client'
 import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/images/logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+const StyledHeader = styled.header``
 
 const Header = () => {
-  return <header>헤더 영역</header>
+  return (
+    <StyledHeader>
+      <div className="inner layout-width">
+        <div className="left"></div>
+        <div className="logo-section">
+          <Link href="/">
+            <Image src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="right">
+          <Link href="/member/join">회원가입</Link>
+          <Link href="/member/login">로그인</Link>
+        </div>
+      </div>
+    </StyledHeader>
+  )
 }
 
 export default React.memo(Header)
