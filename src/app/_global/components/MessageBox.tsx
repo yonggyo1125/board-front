c3836989a0829973158dd7161876f5df1169cfb9
yonggyo1125 <yonggyo00@kakao.com>
2025-08-13 14:49:21 +0900

    .

--- src/app/_global/components/MessageBox.tsx
+++ src/app/_global/components/MessageBox.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import MessageType from '../types/MessageType'
+import color from '../styles/color'
+import fontsize from '../styles/fontsize'
+const { normal } = fontsize
 
-const StyledMessage = styled.div``
+const StyledMessage = styled.div`
+  font-size: ${normal};
+  color: ${({ color: c }) => (c ? color[c] : c)};
+  box-shadow: 2px 2px 5px ${({ color: c }) => (c ? color[c] : c)};
+  padding: 7px 10px;
+  text-align: center;
+  margin-top: 3px;
+  border-radius: 3px;
+`
 
 const MessageBox = ({ children, color, message }: MessageType) => {
   if (children) message = children
   message = Array.isArray(message) ? message : message ? [message] : []
   if (message.length === 0) return <></>
+
+  return message.map((m, i) => (
+    <StyledMessage key={i + '-' + m} color={color ?? 'primary'}>
+      {m}
+    </StyledMessage>
+  ))
 }
 
 export default React.memo(MessageBox)
