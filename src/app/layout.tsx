0c5b11e6fadeda55f140413cde7854da04fc797d
yonggyo1125 <yonggyo00@kakao.com>
2025-08-13 18:01:57 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,20 @@ import './globals.css'
 import Header from './_global/outlines/Header'
 import Footer from './_global/outlines/Footer'
 import StyledComponentsRegistry from './registry'
+import { getLoggedMember } from './member/_services/actions'
 
 export const metadata: Metadata = {
   title: '게시판',
   description: '게시판 설명...',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const member = await getLoggedMember()
+  console.log('member:', member)
   return (
     <html lang="ko">
       <body>
