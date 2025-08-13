cec6d51fbab2afc20c35f3228214cce5fbf1ee75
yonggyo1125 <yonggyo00@kakao.com>
2025-08-13 16:12:44 +0900

    .

--- src/app/member/_components/LoginForm.tsx
+++ src/app/member/_components/LoginForm.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Input } from '@/app/_global/components/Forms'
+import { SubmitButton } from '@/app/_global/components/Buttons'
+import MessageBox from '@/app/_global/components/MessageBox'
+
+const StyledForm = styled.form``
 
 const LoginForm = () => {
-  return <></>
+  return (
+    <StyledForm autoComplete="off">
+      <Input type="text" name="email" placeholder="이메일을 입력하세요." />
+      <Input
+        type="password"
+        name="password"
+        placeholder="비밀번호를 입력하세요."
+      />
+      <SubmitButton type="submit">로그인</SubmitButton>
+    </StyledForm>
+  )
 }
 
 export default React.memo(LoginForm)
--- src/app/member/login/page.tsx
+++ src/app/member/login/page.tsx
@@ -1,3 +1,12 @@
+import LoginContainer from '../_containers/LoginContainer'
+import ContentBox from '@/app/_global/components/ContentBox'
+import { MainTitle } from '@/app/_global/components/TitleBox'
+
 export default function LoginPage() {
-  return <h1>로그인</h1>
+  return (
+    <ContentBox width={420}>
+      <MainTitle center="true">로그인</MainTitle>
+      <LoginContainer />
+    </ContentBox>
+  )
 }
