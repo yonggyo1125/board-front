bd70d2f9cf275c66f842d2904692480b3b62af5f
yonggyo1125 <yonggyo00@kakao.com>
2025-08-18 18:22:57 +0900

    .

--- src/app/recycle/components/DetectObject.tsx
+++ src/app/recycle/components/DetectObject.tsx
@@ -1,14 +1,39 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  video {
+    display: none;
+  }
+`
 
 const DetectObject = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
+  const detect = useCallback(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    canvas.toBlob((blob) => {
+      if (!blob) return
+
+      const formData = new FormData()
+      formData.append('file', blob, 'canvas.jpg')
+
+      fetch(`${process.env.NEXT_PUBLIC_AI_API_URL2}/detect`, {
+        method: 'POST',
+        body: formData,
+      })
+        .then((res) => res.json())
+        .then((items) => console.log('items', items))
+    })
+  }, [canvasRef])
   useEffect(() => {
     const video = videoRef.current
+    const canvas = canvasRef.current
+
+    let videoInterval, detectInterval
 
     navigator.mediaDevices
       .getUserMedia({
@@ -17,8 +42,28 @@ const DetectObject = () => {
       })
       .then((stream) => {
         if (video) video.srcObject = stream
+
+        detectInterval = setInterval(() => {
+          detect()
+        }, 1000)
       })
-  }, [videoRef])
+      .catch((err) => {
+        // 웹캠이 설치되어 있지 않거나, 웹캠 권한을 허용하지 않은 경우..
+        console.log('err', err)
+      })
+
+    const ctx = canvas?.getContext('2d')
+    video?.addEventListener('play', () => {
+      videoInterval = setInterval(() => {
+        ctx?.drawImage(video, 0, 0, canvas?.width ?? 500, canvas?.height ?? 500)
+      }, 0)
+    })
+
+    return () => {
+      clearInterval(videoInterval)
+      clearInterval(detectInterval)
+    }
+  }, [videoRef, canvasRef, detect])
 
   return (
     <Wrapper>
