e87cf050bedb37a8f6dbc4eb6cc32205c88fcf61
yonggyo1125 <yonggyo00@kakao.com>
2025-08-18 13:21:07 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+'use client'
+import { useEffect } from 'react'
+import useFetch from './_global/hooks/useFetch'
 export default function MainPage() {
+  const data = useFetch('http://localhost:4000/api/vi/member')
+  console.log(data)
   return <></>
 }
