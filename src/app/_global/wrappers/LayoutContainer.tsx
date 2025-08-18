1fba6f5ece8c4a2c204528367370953de3be8c4a
yonggyo1125 <yonggyo00@kakao.com>
2025-08-18 15:07:17 +0900

    .

--- src/app/_global/wrappers/LayoutContainer.tsx
+++ src/app/_global/wrappers/LayoutContainer.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { usePathname } from 'next/navigation'
 import useUser from '../hooks/useUser'
 import loadable from '@loadable/component'
 const AdminHeader = loadable(() => import('../outlines/admin/Header'))
@@ -6,3 +7,24 @@ const AdminSide = loadable(() => import('../outlines/admin/Side'))
 
 const Header = loadable(() => import('../outlines/Header'))
 const Footer = loadable(() => import('../outlines/Footer'))
+
+export default function LayoutContainer({ children }) {
+  const { isAdmin } = useUser()
+  const pathname = usePathname()
+
+  return isAdmin && pathname.startsWith('/admin') ? (
+    <>
+      <AdminHeader />
+      <main>
+        <AdminSide />
+        <section className="admin-content">{children}</section>
+      </main>
+    </>
+  ) : (
+    <>
+      <Header />
+      <main className="main-content">{children}</main>
+      <Footer />
+    </>
+  )
+}
