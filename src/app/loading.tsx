ba8f46f90e73dbe974a45f47606f532ca6b71c5e
yonggyo1125 <yonggyo00@kakao.com>
2025-08-18 12:14:55 +0900

    .

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import { ImSpinner4 } from 'react-icons/im'
+
+export default function Loading() {
+  return (
+    <>
+      <h1>로딩중...</h1>
+      <ImSpinner4 />
+    </>
+  )
+}
---
+++ src/app/unauthorized.tsx
@@ -0,0 +1,3 @@
+export default function Unauthorized() {
+  return <h1>접근 권한이 없습니다.</h1>
+}
