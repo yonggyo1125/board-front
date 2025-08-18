5e3ff400a6b038a3dcdbb9ded96cbbcde61d99fe
yonggyo1125 <yonggyo00@kakao.com>
2025-08-18 12:23:45 +0900

    .

--- src/app/_global/components/LinkLoading.tsx
+++ src/app/_global/components/LinkLoading.tsx
@@ -1,1 +1,8 @@
 'use client'
+import { ImSpinner2 } from 'react-icons/im'
+import { useLinkStatus } from 'next/link'
+
+export default function LinkLoading() {
+  const { pending } = useLinkStatus()
+  return pending && <ImSpinner2 className="spinner" />
+}
