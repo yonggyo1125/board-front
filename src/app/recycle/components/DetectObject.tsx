9979fa55a5b5abd8471c02099a8599e0c2f6a286
yonggyo1125 <yonggyo00@kakao.com>
2025-08-19 12:23:32 +0900

    .

--- src/app/recycle/components/DetectObject.tsx
+++ src/app/recycle/components/DetectObject.tsx
@@ -1,19 +1,37 @@
 import React, { useRef, useEffect, useCallback } from 'react'
+import { color, category } from '../libs'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  position: relative;
+
   video {
     display: none;
+  }
+
+  .layer {
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 `
 
 const DetectObject = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const layerRef = useRef<HTMLCanvasElement | null>(null)
 
   const detect = useCallback(() => {
     const canvas = canvasRef.current
-    if (!canvas) return
+    const layer = layerRef.current
+    if (!canvas || !layer) return
+
+    const ctx = layer.getContext('2d')
+    if (!ctx) return
+
+    ctx.lineWidth = 5
+    ctx.strokeStyle = 'red'
+    ctx.clearRect(0, 0, 500, 500)
 
     canvas.toBlob((blob) => {
       if (!blob) return
@@ -26,9 +44,17 @@ const DetectObject = () => {
         body: formData,
       })
         .then((res) => res.json())
-        .then((items) => console.log('items', items))
+        .then((items) => {
+          for (const [x1, y1, x2, y2, , , cls] of items) {
+            const w = Math.abs(x2 - x1)
+            const h = Math.abs(y2 - y1)
+            ctx.strokeRect(x1, y1, w, h)
+            console.log(cls, category[cls])
+          }
+        })
     })
-  }, [canvasRef])
+  }, [canvasRef, layerRef])
+
   useEffect(() => {
     const video = videoRef.current
     const canvas = canvasRef.current
@@ -69,6 +95,12 @@ const DetectObject = () => {
     <Wrapper>
       <video ref={videoRef} width={500} height={500} autoPlay></video>
       <canvas ref={canvasRef} width={500} height={500}></canvas>
+      <canvas
+        className="layer"
+        ref={layerRef}
+        width={500}
+        height={500}
+      ></canvas>
     </Wrapper>
   )
 }
---
+++ src/app/recycle/libs.ts
@@ -0,0 +1,19 @@
+export const color = {
+  can: 'red',
+  glass: 'orange',
+  paper: 'green',
+  plastic: 'blue',
+  recycle: 'indigo',
+  sticker: 'violet',
+  vinyl: 'pink',
+}
+
+export const category = {
+  can: '캔',
+  glass: '유리',
+  paper: '종이',
+  plastic: '플라스틱',
+  recycle: '기타',
+  sticker: '스티커',
+  vinyl: '비닐',
+}
