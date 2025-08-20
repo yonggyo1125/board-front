d39f567aa0b1b400359cd14ab4ccfd6d4cd985fe
yonggyo1125 <yonggyo00@kakao.com>
2025-08-20 16:43:33 +0900

    .

--- src/app/member/social/[channel]/callback/route.ts
+++ src/app/member/social/[channel]/callback/route.ts
@@ -1,6 +1,21 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
+import { redirect } from 'next/navigation'
+import KakaoApi from '../../_services/KakaoApi'
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({})
+  const searchParams = request.nextUrl.searchParams
+  const code = searchParams.get('code')
+  const redirectUrl = searchParams.get('state') ?? '/'
+  try {
+    if (!code) {
+      throw new Error('인증코드 누락')
+    }
+    
+
+    return NextResponse.json({})
+  } catch (err) {
+    console.error(err)
+    redirect(`/member/login?redirectUrl=${redirectUrl}`)
+  }
 }
