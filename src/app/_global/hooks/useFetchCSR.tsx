b76bb0c47503ef47ea288650105aba485d9655d5
yonggyo1125 <yonggyo00@kakao.com>
2025-08-21 15:46:01 +0900

    .

---
+++ src/app/_global/hooks/useFetchCSR.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { getToken, getUserHash } from '../libs/utils'
+
+export default function useFetchCSR(url: string, options: RequestInit = {}) {
+  const [token, setToken] = useState<string | undefined>()
+  const [hash, setHash] = useState<string | number | undefined>()
+
+  useEffect(() => {
+    ;(async () => {
+      setToken(await getToken())
+      setHash(await getUserHash())
+    })()
+  }, [])
+
+  if (token) {
+    options.headers = options.headers ?? {}
+    options.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  if (hash) {
+    options.headers = options.headers ?? {}
+    options.headers['User-Hash'] = hash
+  }
+
+  return fetch(`${process.env.NEXT_PUBLIC_API_URL}${url}`, options)
+}
