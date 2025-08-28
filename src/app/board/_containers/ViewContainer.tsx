36b4db771920bff4255204fb5d6f12c18d37fc0d
yonggyo1125 <yonggyo00@kakao.com>
2025-08-28 17:45:18 +0900

    .

--- src/app/board/_containers/ViewContainer.tsx
+++ src/app/board/_containers/ViewContainer.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
+import React, { useContext, useLayoutEffect } from 'react'
 import loadable from '@loadable/component'
 import type {
   BoardConfigType,
@@ -9,24 +9,14 @@ import type {
 import CommonContainer from '../_wrappers/CommonContainer'
 import BoardView from '../_components/BoardView'
 import CommonContext from '@/app/_global/contexts/CommonContext'
-import { getList } from '../_services/boardData'
-const ListContainer = loadable(() => import('./ListContainer'))
 
 const ViewContainer = ({
   board,
   data,
-  search,
 }: {
   board?: BoardConfigType
   data?: BoardDataType
-  search?: BoardSearchType
 }) => {
-  const [items, setItems] = useState<Array<BoardDataType> | null | undefined>(
-    [],
-  )
-  const [pagination, setPagingation] = useState<any>()
-  search = search ?? {}
-
   const {
     actions: { setMainTitle },
   } = useContext(CommonContext)
@@ -37,23 +27,9 @@ const ViewContainer = ({
     }
   }, [data, board, setMainTitle])
 
-  useEffect(() => {
-    if (board?.showViewList && board?.bid) {
-      ;(async () => {
-        const data = await getList(board.bid, search)
-        const { items, pagination } = data
-        setItems(items)
-        setPagingation(pagination)
-      })()
-    }
-  }, [board?.showViewList, board?.bid, search])
-
   return (
     <CommonContainer board={board} data={data}>
       <BoardView board={board} data={data} />
-      {board?.showViewList && (
-        <ListContainer items={items} pagination={pagination} search={search} />
-      )}
     </CommonContainer>
   )
 }
--- src/app/board/view/[seq]/page.tsx
+++ src/app/board/view/[seq]/page.tsx
@@ -1,21 +1,43 @@
 import ContentBox from '@/app/_global/components/ContentBox'
 import { MainTitle } from '@/app/_global/components/TitleBox'
-import { get } from '../../_services/boardData'
+import { get, getList } from '../../_services/boardData'
 import ViewContainer from '../../_containers/ViewContainer'
+import ListContainer from '../../_containers/ListContainer'
+import type { BoardDataType, BoardSearchType } from '../../_types/BoardType'
 export default async function ViewPage({
   params,
+  searchParams,
 }: {
   params: Promise<{ seq: number }>
+  searchParams: Promise<BoardSearchType>
 }) {
   const { seq } = await params
   const data = await get(seq)
   const { board } = data
   data.mode = 'view'
 
+  const search = await searchParams
+
+  let items: Array<BoardDataType> = [],
+    pagination: any = null
+
+  if (board?.showViewList) {
+    const _data = await getList(board.bid, search)
+    items = _data.items ?? []
+    pagination = _data.pagination
+  }
   return (
     <ContentBox>
       {board?.name && <MainTitle border="true">{board.name}</MainTitle>}
       <ViewContainer board={board} data={data} />
+      {board?.showViewList && (
+        <ListContainer
+          board={board}
+          items={items}
+          pagination={pagination}
+          search={search}
+        />
+      )}
     </ContentBox>
   )
 }
