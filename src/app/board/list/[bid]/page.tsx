b037123488e897e31d0ad92ed81a9d4bee24c3ff
yonggyo1125 <yonggyo00@kakao.com>
2025-08-28 15:22:03 +0900

    .

--- src/app/board/list/[bid]/page.tsx
+++ src/app/board/list/[bid]/page.tsx
@@ -2,7 +2,11 @@ import ContentBox from '@/app/_global/components/ContentBox'
 import { MainTitle } from '@/app/_global/components/TitleBox'
 import { getBoardConfig } from '../../_services/boardConfig'
 import { getList } from '../../_services/boardData'
-import type { BoardConfigType, BoardSearchType } from '../../_types/BoardType'
+import type {
+  BoardConfigType,
+  BoardSearchType,
+  BoardListType,
+} from '../../_types/BoardType'
 import ListContainer from '../../_containers/ListContainer'
 export default async function ListPage({
   params,
@@ -15,12 +19,12 @@ export default async function ListPage({
   const board: BoardConfigType = await getBoardConfig(bid)
   const search = await searchParams
 
-  const data = await getList(bid, search)
-  console.log('data', data)
+  const { items, pagination } = await getList(bid, search)
+
   return (
     <ContentBox>
       <MainTitle border="true">{board.name}</MainTitle>
-      <ListContainer board={board} />
+      <ListContainer board={board} items={items} pagination={pagination} />
     </ContentBox>
   )
 }
