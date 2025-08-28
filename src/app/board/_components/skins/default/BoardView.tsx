bbae77c272ce324f1269a18af7a7438148075c12
yonggyo1125 <yonggyo00@kakao.com>
2025-08-28 12:17:58 +0900

    .

--- src/app/board/_components/skins/default/BoardView.tsx
+++ src/app/board/_components/skins/default/BoardView.tsx
@@ -1,6 +1,33 @@
+'use client'
 import React from 'react'
-const BoardView = () => {
-  return <></>
+import styled from 'styled-components'
+import type { BoardViewType } from '@/app/board/_types/BoardType'
+
+const Wrapper = styled.ul``
+
+const BoardView = ({ board, data }: BoardViewType) => {
+  console.log('data', data)
+  return (
+    data && (
+      <Wrapper>
+        <li className="post-info">
+          <div className="left">
+            <span>
+              작성자: {data.poster}
+              {data.member && '(' + data.member.email + ')'}
+            </span>
+            <span>IP: {data.ip}</span>
+          </div>
+          <div className="right">
+            {data.viewCount && (
+              <span>조회수: {data.viewCount.toLocaleString()}</span>
+            )}
+            <span>작성일시: </span>
+          </div>
+        </li>
+      </Wrapper>
+    )
+  )
 }
 
 export default React.memo(BoardView)
--- src/app/board/_containers/ViewContainer.tsx
+++ src/app/board/_containers/ViewContainer.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React from 'react'
 import type { BoardConfigType, BoardDataType } from '../_types/BoardType'
+import CommonContainer from '../_wrappers/CommonContainer'
+import BoardView from '../_components/BoardView'
 const ViewContainer = ({
   board,
   data,
@@ -8,7 +10,11 @@ const ViewContainer = ({
   board?: BoardConfigType
   data?: BoardDataType
 }) => {
-  return <></>
+  return (
+    <CommonContainer board={board} data={data}>
+      <BoardView board={board} data={data} />
+    </CommonContainer>
+  )
 }
 
 export default React.memo(ViewContainer)
