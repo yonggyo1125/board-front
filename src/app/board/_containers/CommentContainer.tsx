bc232f5f08ac879674d9690746385dc73fcf3789
yonggyo1125 <yonggyo00@kakao.com>
2025-08-29 16:18:53 +0900

    .

--- src/app/board/_containers/CommentContainer.tsx
+++ src/app/board/_containers/CommentContainer.tsx
@@ -1,10 +1,25 @@
 'use client'
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Comment from '../_components/Comment'
-import type { CommentType } from '../_types/CommentType'
+import type { CommentType, CommentDataType } from '../_types/CommentType'
+import useUser from '@/app/_global/hooks/useUser'
 
-const CommentContainer = ({ board }: CommentType) => {
-  return <Comment board={board} />
+const CommentContainer = ({ board, data }: CommentType) => {
+  const { isLogin, loggedMember } = useUser()
+  const [form, setForm] = useState<CommentDataType>({
+    mode: 'comment_write',
+    boardDataSeq: data?.seq,
+    commenter: loggedMember?.name ?? '',
+    guestPw: '',
+    content: '',
+    guest: !isLogin,
+  })
+
+  const onChange = useCallback((e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }, [])
+
+  return <Comment board={board} data={data} form={form} onChange={onChange} />
 }
 
 export default React.memo(CommentContainer)
--- src/app/board/_types/CommentType.ts
+++ src/app/board/_types/CommentType.ts
@@ -10,6 +10,7 @@ export type CommentDataType = {
   mode?: 'comment_write' | 'comment_update' | 'comment_delete'
   boardDataSeq?: number // 게시글 번호
   seq?: number // 댓글 번호
+  commenter?: string
   guestPw?: string
   content: string
   item?: BoardDataType // 원 게시글
