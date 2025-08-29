d3b45c8626c509e2917264fe09fbc37b47b7437c
yonggyo1125 <yonggyo00@kakao.com>
2025-08-29 11:02:37 +0900

    .

---
+++ src/app/board/_components/PasswordForm.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledForm = styled.form``
+
+const PasswordForm = () => {
+  return <StyledForm>1111</StyledForm>
+}
+
+export default React.memo(PasswordForm)
---
+++ src/app/board/_containers/PasswordContainer.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+import PasswordForm from '../_components/PasswordForm'
+
+const PasswordContainer = () => {
+  return <PasswordForm />
+}
+
+export default React.memo(PasswordContainer)
